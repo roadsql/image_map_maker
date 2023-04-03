dd3ffce578b06915ff67041e89209a7da30f9183
jade-addin <jade.addin@gmail.com>
2023-04-03 06:17:37 -0600

    progress

--- config_sample.js
+++ config_sample.js
@@ -2,4 +2,7 @@
 // ids to refererence the correct google apps script endpoint and 
 // sheet it.  name it config.js and be sure it is gitignore
 const deployment_id="deployment id of the google app script backend"
-const sheet_id = "sheet ID that holds the data and metadata for the diagram"
+const sheet_ids ={
+    canvas:"sheet ID that holds the data and metadata for the diagram",
+    registrar:"sheet ID that holds the data and metadata for the diagram"
+} 
